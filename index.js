b729cad3fc2ddb5693547797c1942eb0c13eeea9
Alan Dávalos <gdavalos@neodata.com.mx>
2016-12-22 17:30:53 -0600

    Generar css

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const OAuth2 = require('oauth').OAuth2;
 const Twitter = require('twitter');
+const moment = require('moment');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -27,19 +28,32 @@ app.get('/twitter.css', function(request, response) {
       client.get('search/tweets.json', params)
         .then(function (tweets){
           let statuses = tweets.statuses;
-          let css = [];
+          let css = ['@media screen and (-webkit-min-device-pixel-ratio: 0) {',
+            '.tweet .copy:before {white-space: pre-wrap;',
+            '}'];
           for (let i = 0; i < statuses.length; i++){
             let user = statuses[i].user;
             let avatar = user.profile_image_url_https;
             let name = user.name;
             let handle = user.screen_name;
-            let timestamp = statuses[i].created_at;
-            let copy = statuses[i].text;
-            let info = {avatar: avatar, name: name, handle: handle,
-              timestamp: timestamp, copy: copy};
-            css.push(info);
+            let timestamp = moment(statuses[i].created_at).locale('es-MX').format('DD MMM');
+            let copy = statuses[i].text.split('\n').join(' ');
+            let id = '#tweet-' + (i + 1);
+
+            let avatarcss = [id, ' .avatar {', 'background: url("', avatar, '");}'].join('');
+            let namecss = [id, ' .name::before { content: "', name, '";}'].join('');
+            let handlecss = [id, ' .handle::after { content: "', handle, '";}'].join('');
+            let copycss = [id, ' .copy::before { content: "', copy, '";}'].join('');
+            let timestampcss = [id, '.timestamp::after { content: "',
+              timestamp, '";}'].join('');
+            css.push(avatarcss);
+            css.push(namecss);
+            css.push(handlecss);
+            css.push(copycss);
+            css.push(timestampcss);
           }
-          response.send(css);
+          response.setHeader('content-type', 'text/css');
+          response.send(css.join(''));
         })
         .catch(function (error){
           throw error;
